016da8d0f8d1abe9b890bc37c54cb07d1f7976da
AnastasiiaHudymenko <106466791+AnastasiiaHudymenko@users.noreply.github.com>
2022-12-24 19:00:59 +0200

    add js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const refs = {
   day: document.querySelector('.page-main__day-title'),
   date: document.querySelector('.page-main__day-of-month'),
   city: document.querySelector('.page-main__location-now'),
+  currentTemp: document.querySelector('.page-main__degree'),
+  currentStutusWth: document.querySelector('.page-main__status-weather'),
+  cloudniness: document.querySelector('.list-decs__info--cld'),
+  humidity: document.querySelector('.list-decs__info--hum'),
+  wind: document.querySelector('.list-decs__info--wind'),
 };
 
 const dayArr = [
@@ -14,19 +19,17 @@ const dayArr = [
   'Saturday',
 ];
 
-// const API_KEY = 'fec120b35dbf88575ec32786c3b642e3';
-
-const promise = fetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=Egypt&appid=${API_KEY}`
-);
-
-promise.then(response => response.json()).then(data => create(data));
+const API_KEY = 'fec120b35dbf88575ec32786c3b642e3';
+getFeatch()
+  .then(response => response.json())
+  .then(data => create(data));
+function getFeatch() {
+  return fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=metric&q=Kiev&appid=${API_KEY}`
+  );
+}
 
 function create(data) {
-  data.weather.forEach(el => {
-    console.log(el.icon);
-  });
-
   const currentDate = new Date();
   const day = currentDate.getDay();
 
@@ -36,4 +39,11 @@ function create(data) {
     0,
     3
   )} ${currentDate.getFullYear()}`;
+  refs.currentTemp.textContent = `${Math.round(data.main.temp)}°C`;
+  data.weather.forEach(({ main }) => {
+    refs.currentStutusWth.textContent = `${main}`;
+  });
+  refs.cloudniness.textContent = `${data.clouds.all} %`;
+  refs.humidity.textContent = `${data.main.humidity} %`;
+  refs.wind.textContent = `${(data.wind.speed * 3.6).toFixed(1)} km/h`;
 }
